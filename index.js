87c66bacdfc748566a34317ae99e1c6358b16d05
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2025-12-16 14:06:30 -0600

    Fix markAsDone function to iterate correctly

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 let todos = ['first todo', 'second todo', 'third todo']
 
 function markAsDone() {
-  for(let i = 0; i < 4; i++) {
+  for(let i = 0; i < 3; i++) {
     todos[i] = 'done - ' + todos[i];
-    return todos;
   }
+  return todos;
 }
-
-
-
